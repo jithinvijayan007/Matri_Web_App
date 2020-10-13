4d7fa4442fc1bf1fa6c1b0dad9b555a7529c6cd7
Jithin <67956333+jithinvijayan007@users.noreply.github.com>
2020-10-13 21:33:50 +0530

    Update settings.py

--- matri/settings.py
+++ matri/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersCOnfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
